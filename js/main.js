aca2aca3129f7d7bf589ed96a56e2ce43a86b007
Rushikesh Kailas Badgujar <rushikeshkailasbadgujar@Rushikeshs-MacBook-Air.local>
2025-12-28 15:15:58 +0530

    added the carasol in the blog pages

--- js/main.js
+++ js/main.js
@@ -60,6 +60,33 @@
             '<i class="bi bi-chevron-right"></i>'
         ]
     });
+
+
+    // Blog carousel
+    $(".blog-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        center: false,
+        dots: false,
+        loop: true,
+        margin: 25,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
     
 })(jQuery);
 
